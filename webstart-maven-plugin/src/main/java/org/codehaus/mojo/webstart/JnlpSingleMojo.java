87a091dd45cccfc976600a4e671425f54acdf2e9
Milos Kleint <mkleint@gmail.com>
2008-04-21 07:10:41 +0000

    remove unused imports
    git-svn-id: file:///home/tiste/MOJOHAUS-TO-GIT/SVN-MOJO-WIP/trunk/mojo/webstart@6802 52ab4f32-60fc-0310-b215-8acea882cd1b

--- webstart-maven-plugin/src/main/java/org/codehaus/mojo/webstart/JnlpSingleMojo.java
+++ webstart-maven-plugin/src/main/java/org/codehaus/mojo/webstart/JnlpSingleMojo.java
@@ -16,40 +16,7 @@ package org.codehaus.mojo.webstart;
  * limitations under the License.
  */
 
-import org.apache.maven.artifact.Artifact;
-import org.apache.maven.artifact.factory.ArtifactFactory;
-import org.apache.maven.artifact.repository.ArtifactRepository;
-import org.apache.maven.artifact.resolver.ArtifactResolver;
-import org.apache.maven.artifact.resolver.filter.AndArtifactFilter;
-import org.apache.maven.artifact.resolver.filter.IncludesArtifactFilter;
-import org.apache.maven.artifact.resolver.filter.ExcludesArtifactFilter;
-import org.apache.maven.plugin.AbstractMojo;
-import org.apache.maven.plugin.MojoExecutionException;
-import org.apache.maven.plugin.PluginManager;
-import org.apache.maven.plugin.jar.JarSignMojo;
 import org.apache.maven.project.MavenProject;
-import org.apache.maven.project.MavenProjectHelper;
-import org.apache.maven.settings.Settings;
-
-import org.codehaus.mojo.keytool.GenkeyMojo;
-import org.codehaus.mojo.webstart.generator.Generator;
-import org.codehaus.plexus.archiver.manager.ArchiverManager;
-import org.codehaus.plexus.archiver.zip.ZipArchiver;
-import org.codehaus.plexus.util.FileUtils;
-import org.codehaus.plexus.util.DirectoryScanner;
-
-import org.apache.commons.lang.SystemUtils;
-
-import java.io.File;
-import java.io.FileFilter;
-import java.io.IOException;
-import java.net.URL;
-import java.net.MalformedURLException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
 
 /**
  * Packages a jnlp application without launching a parallel lifecycle build.
