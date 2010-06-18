dd22b39db5683f576f7be18560827f88650325f3
Robert Scholte <rfscholte@apache.org>
2010-06-18 19:56:21 +0000

    Fixed checkstyle errors for webstart-jarsigner-api
    git-svn-id: file:///home/tiste/MOJOHAUS-TO-GIT/SVN-MOJO-WIP/trunk/mojo/webstart@12278 52ab4f32-60fc-0310-b215-8acea882cd1b

--- webstart-jarsigner-api/src/main/java/org/codehaus/mojo/webstart/JarSignerMojo.java
+++ webstart-jarsigner-api/src/main/java/org/codehaus/mojo/webstart/JarSignerMojo.java
@@ -1,20 +1,24 @@
 package org.codehaus.mojo.webstart;
 
 /*
- * Copyright 2001-2007 The Apache Software Foundation.
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
  *
- * Licensed under the Apache License, Version 2.0 (the "License" );
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
+
 
 import java.io.File;
 
@@ -41,21 +45,22 @@ import org.apache.maven.plugin.MojoFailureException;
  * use JarSignMojo as a default) so that the user can plug in a custom
  * Mojo to sign their jars.
  */
-public interface JarSignerMojo {
+public interface JarSignerMojo 
+{
     /**
      * Sets the location of the unsigned jar file.
-     * @param jarPath
+     * @param jarPath path to jar
      */
-    void setJarPath(File jarPath);
+    void setJarPath( File jarPath );
     
     /**
      * Sets the output filename for the signed jar.
      * This may be the same location as specified in setJarPath(). If this
      * is the case, the unsigned jar file will be overwritten with the
      * signed jar file.
-     * @param signedJar
+     * @param signedJar the signed jar
      */
-    void setSignedJar(File signedJar);
+    void setSignedJar( File signedJar );
     
     /**
      * Executes the jar signing process.
