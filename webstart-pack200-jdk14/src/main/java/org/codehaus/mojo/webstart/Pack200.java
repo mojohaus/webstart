5362f4772ef14cc72457c14141c5f4d4271a0395
Robert Scholte <rfscholte@apache.org>
2010-06-18 20:15:13 +0000

    Fixed checkstyle errors for webstart-pack200-*
    git-svn-id: file:///home/tiste/MOJOHAUS-TO-GIT/SVN-MOJO-WIP/trunk/mojo/webstart@12279 52ab4f32-60fc-0310-b215-8acea882cd1b

--- webstart-pack200-jdk14/src/main/java/org/codehaus/mojo/webstart/Pack200.java
+++ webstart-pack200-jdk14/src/main/java/org/codehaus/mojo/webstart/Pack200.java
@@ -1,19 +1,22 @@
 package org.codehaus.mojo.webstart;
 
 /*
- * Copyright 2001-2005 The Apache Software Foundation.
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
  *
- * Licensed under the Apache License, Version 2.0 (the "License" );
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 
 import java.io.File;
@@ -29,11 +32,13 @@ public class Pack200
 
     public static void packJars( File directory, FileFilter jarFileFilter, boolean gzip )
     {
-        throw new IllegalStateException( "Pack200 doesn't exist for SDK 1.4. Should never be called. Compilation stub only" );
+        throw new IllegalStateException( "Pack200 doesn't exist for SDK 1.4. Should never be called. " 
+                                         + "Compilation stub only" );
     }
 
     public static void unpackJars( File directory, FileFilter pack200FileFilter )
     {
-        throw new IllegalStateException( "Pack200 doesn't exist for SDK 1.4. Should never be called. Compilation stub only" );
+        throw new IllegalStateException( "Pack200 doesn't exist for SDK 1.4. Should never be called. " 
+                                         + "Compilation stub only" );
     }
 }
