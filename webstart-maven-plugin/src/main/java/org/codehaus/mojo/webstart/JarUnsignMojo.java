bb63c8e5b15ee1cbf0492d2d0cc386d8cf5e000c
Tony Chemit <tchemit@apache.org>
2011-11-20 18:37:47 +0000

    use the SignTool
    git-svn-id: file:///home/tiste/MOJOHAUS-TO-GIT/SVN-MOJO-WIP/trunk/mojo/webstart@15186 52ab4f32-60fc-0310-b215-8acea882cd1b

--- webstart-maven-plugin/src/main/java/org/codehaus/mojo/webstart/JarUnsignMojo.java
+++ webstart-maven-plugin/src/main/java/org/codehaus/mojo/webstart/JarUnsignMojo.java
@@ -80,32 +80,6 @@ public class JarUnsignMojo
      */
     private SignTool signTool;
 
-//    /**
-//     * @component
-//     */
-//    private MavenProjectHelper projectHelper;
-//
-//    /**
-//     * To look up Archiver/UnArchiver implementations
-//     *
-//     * @component
-//     * @required
-//     */
-//    protected ArchiverManager archiverManager;
-
-//    private static final String[] EXT_ARRAY = { "DSA", "RSA", "SF" };
-//
-//    private FileFilter removeSignatureFileFilter = new FileFilter()
-//    {
-//        private final List extToRemove = Arrays.asList( EXT_ARRAY );
-//
-//        public boolean accept( File file )
-//        {
-//            String extension = FileUtils.getExtension( file.getAbsolutePath() );
-//            return ( extToRemove.contains( extension ) );
-//        }
-//    };
-
     public void execute()
         throws MojoExecutionException
     {
@@ -116,124 +90,7 @@ public class JarUnsignMojo
         }
 
         signTool.unsign( this.jarPath, this.tempDirectory, verbose );
-
-//        File jarFile = this.jarPath;
-//        File tempDirParent = this.tempDirectory;
-//
-//        String archiveExt = FileUtils.getExtension( jarFile.getAbsolutePath() ).toLowerCase();
-//
-//        // create temp dir
-//        File tempDir = new File( tempDirParent, jarFile.getName() );
-//
-//        if ( !tempDir.mkdirs() )
-//        {
-//            throw new MojoExecutionException( "Error creating temporary directory: " + tempDir );
-//        }
-//        // FIXME we probably want to be more security conservative here.
-//        // it's very easy to guess where the directory will be and possible
-//        // to access/change its contents before the file is rejared..
-//
-//        // extract jar into temporary directory
-//        try
-//        {
-//            UnArchiver unArchiver = this.archiverManager.getUnArchiver( archiveExt );
-//            unArchiver.setSourceFile( jarFile );
-//            unArchiver.setDestDirectory( tempDir );
-//            unArchiver.extract();
-//        }
-//        catch ( ArchiverException ex )
-//        {
-//            throw new MojoExecutionException( "Error unpacking file: " + jarFile + "to: " + tempDir, ex );
-//        }
-//        catch ( NoSuchArchiverException ex )
-//        {
-//            throw new MojoExecutionException( "Error acquiring unarchiver for extension: " + archiveExt, ex );
-//        }
-//
-//        // create and check META-INF directory
-//        File metaInf = new File( tempDir, "META-INF" );
-//        if ( !metaInf.isDirectory() )
-//        {
-//            verboseLog( "META-INT dir not found : nothing to do for file: " + jarPath.getAbsolutePath() );
-//            return;
-//        }
-//
-//        // filter signature files and remove them
-//        File[] filesToRemove = metaInf.listFiles( this.removeSignatureFileFilter );
-//        if ( filesToRemove.length == 0 )
-//        {
-//            verboseLog(
-//                "no files match " + toString( EXT_ARRAY ) + " : nothing to do for file: " + jarPath.getAbsolutePath() );
-//            return;
-//        }
-//        for ( int i = 0; i < filesToRemove.length; i++ )
-//        {
-//            if ( !filesToRemove[i].delete() )
-//            {
-//                throw new MojoExecutionException( "Error removing signature file: " + filesToRemove[i] );
-//            }
-//            verboseLog( "remove file :" + filesToRemove[i] );
-//        }
-//
-//        // recreate archive
-//        try
-//        {
-//            JarArchiver jarArchiver = (JarArchiver) this.archiverManager.getArchiver( "jar" );
-//            jarArchiver.setUpdateMode( false );
-//            jarArchiver.addDirectory( tempDir );
-//            jarArchiver.setDestFile( jarFile );
-//            jarArchiver.createArchive();
-//
-//        }
-//        catch ( ArchiverException ex )
-//        {
-//            throw new MojoExecutionException( "Error packing directory: " + tempDir + "to: " + jarFile, ex );
-//        }
-//        catch ( IOException ex )
-//        {
-//            throw new MojoExecutionException( "Error packing directory: " + tempDir + "to: " + jarFile, ex );
-//        }
-//        catch ( NoSuchArchiverException ex )
-//        {
-//            throw new MojoExecutionException( "Error acquiring archiver for extension: jar", ex );
-//        }
-//
-//        try
-//        {
-//            FileUtils.deleteDirectory( tempDir );
-//        }
-//        catch ( IOException ex )
-//        {
-//            throw new MojoExecutionException( "Error cleaning up temporary directory file: " + tempDir, ex );
-//        }
     }
-
-//    /**
-//     * Log as info when verbose or info is enabled, as debug otherwise.
-//     * @param msg message to log
-//     */
-//    protected void verboseLog( String msg )
-//    {
-//        infoOrDebug( isVerbose() || getLog().isInfoEnabled(), msg );
-//    }
-//
-//    /**
-//     * Log a message as info or debug.
-//     *
-//     * @param info if set to true, log as info(), otherwise as debug()
-//     * @param msg message to log
-//     */
-//    private void infoOrDebug( boolean info, String msg )
-//    {
-//        if ( info )
-//        {
-//            getLog().info( msg );
-//        }
-//        else
-//        {
-//            getLog().debug( msg );
-//        }
-//    }
 
 
     public void setTempDir( File tempDirectory )
@@ -255,30 +112,5 @@ public class JarUnsignMojo
     {
         this.signTool = signTool;
     }
-
-//    public boolean isVerbose()
-//    {
-//        return verbose;
-//    }
-
-//    public void setArchiverManager( ArchiverManager archiverManager )
-//    {
-//        this.archiverManager = archiverManager;
-//    }
-
-//    public String toString( String[] items )
-//    {
-//        StringBuffer back = new StringBuffer( "{" );
-//        for ( int i = 0; i < items.length; i++ )
-//        {
-//            if ( i != 0 )
-//            {
-//                back.append( ", " );
-//            }
-//            back.append( '"' ).append( items[i] ).append( '"' );
-//        }
-//        back.append( "}" );
-//        return back.toString();
-//    }
 
 }
